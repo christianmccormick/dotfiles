b26a62af6c68386cf031cd4db82c6781375e769c
Christian McCormick <cmccormick421@gmail.com>
2025-06-24 13:28:30 -0700

    nvim: don't consider parent directories with stylua config lua root dirs

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,6 +5,22 @@ return {
       virtual_text = false,
     },
     servers = {
+      lua_ls = {
+        root_dir = function(fname)
+          local lspconfig = require("lspconfig")
+          local default_root_dir = lspconfig.lua_ls.document_config.default_config.root_dir
+          local root = default_root_dir(fname)
+
+          if
+            root and vim.fn.filereadable(root .. "/.stylua.toml") == 1
+            or vim.fn.filereadable(root .. "/stylua.toml") == 1
+          then
+            return nil
+          end
+
+          return root
+        end,
+      },
       ruby_lsp = {
         init_options = {
           formatter = "syntax_tree",
