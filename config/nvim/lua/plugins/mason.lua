141845387fabd177ae52689453063927208690c9
Christian McCormick <cmccormick421@gmail.com>
2025-10-01 09:42:59 -0700

    nvim: upgrade mason to 2.x
    I just upgraded LazyVim to v15. This new version has support for mason
    v2, so this forced installing of v1 can be removed.

--- config/nvim/lua/plugins/mason.lua
+++
@@ -1,6 +0,0 @@
--- Workaround as mason 2.x is not yet compatible with neovim
--- Reference: https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
-return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
