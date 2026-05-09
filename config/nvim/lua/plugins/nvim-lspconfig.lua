c27a4e02808c3b95dc4372a681ef3c82d35b4816
Christian McCormick <cmccormick421@gmail.com>
2026-05-09 11:00:23 -0700

    nvim: remove copilot lspconfig

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -8,13 +8,6 @@ return {
       astro = {},
       cssls = {},
       css_variables = {},
-      copilot = {
-        settings = {
-          telemetry = {
-            telemetryLevel = "off",
-          },
-        },
-      },
       lua_ls = {
         root_dir = function(fname)
           local lspconfig = require("lspconfig")
